2f718f3d14508296276552aaa44f1c44d9845764
Max Alva <alva85@gmail.com>
2019-03-30 23:58:03 -0500

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:introduccion_flutter/widgets/alert.dart';
-import 'package:introduccion_flutter/widgets/navigator-tabs.dart';
-import 'package:introduccion_flutter/widgets/stepper.dart';
-import 'package:introduccion_flutter/widgets/tabs.dart';
+import 'package:introduccion_flutter/widgets/navigator.dart';
 
 void main() {
     runApp(MaterialApp(
 //        home: MyTabs(),
 //    home: MyAlertDialog(),
 //    home: MyStepper(),
-    home: NavigatorTabs(),
+    home: FirstPage(),
     ));
 }
